0d663cc6429a1f37fa888290ca2b867be8bc6eb1
belyaikin <dmitriybelyaikin@gmail.com>
2025-10-27 14:54:45 +0500

    Implemented Json file creation

--- src/main/java/me/belyaikin/primskruskals/Main.java
+++ src/main/java/me/belyaikin/primskruskals/Main.java
@@ -1,42 +1,21 @@
 package me.belyaikin.primskruskals;
 
 import me.belyaikin.primskruskals.json.GraphParser;
+import me.belyaikin.primskruskals.json.JsonFileProcessor;
+
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        String json = """
-                {
-                  "graphs": [
-                    {
-                      "id": 0,
-                      "vertices": ["A", "B", "C", "D", "E"],
-                      "edges": [
-                        {"from": "A", "to": "B", "weight": 4},
-                        {"from": "A", "to": "C", "weight": 3},
-                        {"from": "B", "to": "C", "weight": 2},
-                        {"from": "B", "to": "D", "weight": 5},
-                        {"from": "C", "to": "D", "weight": 7},
-                        {"from": "C", "to": "E", "weight": 8},
-                        {"from": "D", "to": "E", "weight": 6}
-                      ]
-                    },
-                    {
-                      "id": 1,
-                      "vertices": ["A", "B", "C", "D"],
-                      "edges": [
-                        {"from": "A", "to": "B", "weight": 1},
-                        {"from": "A", "to": "C", "weight": 4},
-                        {"from": "B", "to": "C", "weight": 2},
-                        {"from": "C", "to": "D", "weight": 3},
-                        {"from": "B", "to": "D", "weight": 5}
-                      ]
-                    }
-                  ]
-                }
-                """;
+    public static void main(String[] args) throws IOException {
+        AlgorithmTester tester = new AlgorithmTester(
+                new GraphParser(
+                        JsonFileProcessor.getInputJsonFile("input.json")
+                )
+        );
 
-        AlgorithmTester tester = new AlgorithmTester(new GraphParser(json));
-
-        System.out.println(tester.getExecutionResults());
+        JsonFileProcessor.createResultsJsonFile(
+                "output.json",
+                tester.getExecutionResults().toString()
+        );
     }
 }
---
+++ src/main/java/me/belyaikin/primskruskals/json/JsonFileProcessor.java
@@ -0,0 +1,44 @@
+package me.belyaikin.primskruskals.json;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class JsonFileProcessor {
+    public static String getInputJsonFile(String path) {
+        try {
+            return Files.readString(Paths.get(path));
+        } catch (IOException e) {
+            System.out.println("Error accessing the file: " + e.getMessage());
+        }
+
+        return null;
+    }
+
+    public static void createResultsJsonFile(String path, String json) {
+        File jsonFile = new File(path);
+
+        try {
+            if (jsonFile.createNewFile()) {
+                try {
+                    FileWriter writer = new FileWriter(path);
+
+                    writer.write(json);
+                    writer.close();
+                } catch (IOException e) {
+                    System.out.println("Failed to write to a file: " + e.getMessage());
+                }
+                System.out.println("Successfully recorded results to: " + path);
+            }
+            else {
+                System.out.println("Failed to record results to: " + path);
+            }
+        } catch (IOException e) {
+            System.out.println("Failed to create a new file: " + e.getMessage());
+        }
+    }
+}
